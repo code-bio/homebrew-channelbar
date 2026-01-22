8fbe726b601fc9119c59efbdf33112c95d7767f7
Christian Franzl <github_1312.cf@admonitus.de>
2026-01-22 23:57:40 +0100

    Initial commit

---
+++ Formula/channelbar.rb
@@ -0,0 +1,55 @@
+class Channelbar < Formula
+  desc "CLI tool for channelBar"
+  homepage "https://channelbar.app"
+  license "Copyright © 2026 code.bio GmbH"
+
+  url "https://github.com/code-bio/homebrew-channelbar/releases/download/v1.1.2/channelbar-cli-1.1.2.tar.gz"
+  sha256 "701e0a9f750e4d41931e9ce5014e2ecb2d2dbfb3b4260e1a59d0f458efd680b7"
+
+  depends_on :macos
+
+  def install
+    libexec.mkpath
+  end
+
+  def post_install
+    tarball_url = "https://github.com/code-bio/homebrew-channelbar/releases/download/v#{version}/channelbar-cli-#{version}.tar.gz"
+
+    require "tempfile"
+    tmpfile = Tempfile.new(["channelbar-cli", ".tar.gz"])
+    tmppath = tmpfile.path
+    tmpfile.close
+
+    system "rm", "-rf", libexec/"channelbar", libexec/"Frameworks", libexec/"THIRD-PARTY-LICENSES.txt"
+    system "curl", "-L", tarball_url, "-o", tmppath
+    system "tar", "-xzf", tmppath, "-C", libexec
+    system "rm", "-f", tmppath
+    system "chmod", "755", libexec/"channelbar"
+
+    system "ln", "-sf", libexec/"channelbar", HOMEBREW_PREFIX/"bin/channelbar"
+
+    license_src = libexec/"THIRD-PARTY-LICENSES.txt"
+    if File.exist?(license_src)
+      doc.mkpath
+      system "cp", "-p", license_src, doc/"THIRD-PARTY-LICENSES.txt"
+    end
+  end
+
+  def caveats
+    <<~EOS
+
+      The channelbar CLI requires the channelBar app to be running.
+      Get the app from: https://channelbar.app
+
+      Usage:
+        channelbar version               # Show current version
+        channelbar                       # Show all commands
+        channelbar --licenses            # Show third-party licenses
+
+    EOS
+  end
+
+  test do
+    assert_match(/channelbar|version/i, shell_output("#{bin}/channelbar version", 0))
+  end
+end
---
+++ README.md
@@ -0,0 +1,56 @@
+# Homebrew Tap for channelBar CLI
+
+This is the official Homebrew tap for [channelBar](https://channelbar.app) CLI.
+
+## Installation
+
+```bash
+# Add the tap
+brew tap code-bio/channelbar
+
+# Install the CLI
+brew install channelbar
+```
+
+Or install directly with a single command:
+
+```bash
+brew install code-bio/channelbar/channelbar
+```
+
+## Usage
+
+```bash
+# Show installed version
+channelbar version
+```
+
+## Requirements
+
+- macOS 13.0 or later
+- [channelBar app](https://channelbar.app) installed and running
+
+## Updating
+
+```bash
+brew upgrade channelbar
+```
+
+## Uninstalling
+
+```bash
+brew uninstall channelbar
+brew untap code-bio/channelbar
+```
+
+## About channelBar
+
+channelBar displays customizable messages in your macOS menu bar. Use the CLI to programmatically set messages from scripts, automations, or other tools.
+
+- Website: [channelbar.app](https://channelbar.app)
+- Documentation: [channelbar.app/docs](https://channelbar.app/docs)
+
+## License
+
+Copyright © 2026 code.bio GmbH
+All rights reserved.
